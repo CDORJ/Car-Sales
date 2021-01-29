df3753dd3f9f583cf329143f3b8540e50dd26722
ORiveraJr84 <oriverajr84@gmail.com>
2021-01-29 12:01:31 -0500

    Initial Commit.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
+
+const reducer = () => {
+  return {
+    somedata: "something",
+  };
+};
+const store = createStore(reducer);
+
 ReactDOM.render(<App />, rootElement);
