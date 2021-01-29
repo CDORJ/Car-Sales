baed63dfcb8e6613ddf352369f1594218e96b5c6
ORiveraJr84 <oriverajr84@gmail.com>
2021-01-29 14:53:50 -0500

    Set up initial store and connected App.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
@@ -15,4 +16,9 @@ const reducer = () => {
 };
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
